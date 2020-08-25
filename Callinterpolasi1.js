e8e67e37c4286153f3c20a98b8299f5b37e95644
AmandaApril <68797138+AmandaApril@users.noreply.github.com>
2020-08-25 10:03:46 +0700

    Add files via upload

---
+++ Callinterpolasi1.js
@@ -0,0 +1,13 @@
+function kali(a,b){
+    return a*b;
+}
+
+let a = 10;
+console.log("Masukan Nilai a:" +a);
+let b = 5;
+console.log("Masukkan Nilai b:" + b);
+let c;
+//memanggil fungsi dan menampung ke c
+c=kali(a,b);
+
+console.log(`${a}X${b}=${c}`);
